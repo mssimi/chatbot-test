81d9633d1969bdab8a870921336da26c3a2becf4
mssimi <mssimi@seznam.cz>
2018-02-11 12:44:09 +0100

    added tests

---
+++ tests/ReplyManagerTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Tests;
+
+use App\Answerer\Answerer;
+use App\ReplyManager;
+use PHPUnit\Framework\TestCase;
+
+class ReplyManagerTest extends TestCase
+{
+    /** @var ReplyManager */
+    private $replyManager;
+
+    protected function setUp(): void
+    {
+        $this->replyManager = new ReplyManager();
+    }
+
+    public function test(): void
+    {
+        $nlpAnswererMock = $this->createMock(Answerer::class);
+        $nlpAnswererMock
+            ->expects($this->once())
+            ->method('reply');
+
+        $this->replyManager->addAnswerer($nlpAnswererMock);
+        $this->replyManager->reply();
+    }
+}
